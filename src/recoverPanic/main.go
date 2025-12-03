804a4c9b2464502559de5a86a5adf6419c254cea
zhengyk <763716167@qq.com>
2025-12-03 11:25:37 +0800

    recover panic balabala

---
+++ src/recoverPanic/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer func() {
+		if error := recover(); error != nil {
+			fmt.Println("出现了panic,使用reover获取信息:", error)
+		}
+	}()
+	fmt.Println("11111111111")
+	panic("出现panic")
+	fmt.Println("22222222222")
+}
