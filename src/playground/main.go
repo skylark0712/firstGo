ec3a184d8d42e76128b32c011c2835d5428e027b
zhengyk <763716167@qq.com>
2025-12-04 17:07:27 +0800

    周四跑步继续加油

--- src/playground/main.go
+++ src/playground/main.go
@@ -1,7 +1,19 @@
+// https://leetcode.cn/problems/count-collisions-on-a-road
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Printf("咔咔啦咔")
+	fmt.Println(countCollisions("kklk"))
+}
+
+func countCollisions(directions string) int {
+	n := len(directions)
+	if n <= 1 {
+		return 0
+	}
+	// stopped := make([]bool, n)
+	// 不想写了，开摆
+	total := 0
+	return total
 }
